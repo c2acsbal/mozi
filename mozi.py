85aa7498ac91f8bfc151bc87e75dd32d898a6967
c2acsbal <c2acsbal@vasvari.org>
2025-01-28 11:48:10 +0100

    dolgozgattam

--- mozi.py
+++ mozi.py
@@ -5,16 +5,16 @@ nezoterjobb = []
 sor = []
 sor2 = []
 def feltolto():
-    while len(nezoterbal) != 10:
+    while len(nezoterbal) != 15:
         for i in range(10):
             randi = random.randint(0,4)
             sor.append(randi)
 
         nezoterbal.append(sor)
-    while len(nezoterjobb) != 10:
+    while len(nezoterjobb) != 15:
         for t in range(10):
             randi2 = random.randint(0,4)
-            sor2.append(randi)
+            sor2.append(randi2)
         
         nezoterjobb.append(sor2)
     
@@ -22,39 +22,12 @@ def feltolto():
     teljes.append(nezoterjobb)
     return teljes
 
-feltolto()
-print("egymasmelle?")
-egymasmelle = input()
-egymasmelleakar = False
-if egymasmelle == "i":
-    egymasmelleakar = True
-    print("egymásmellé mennyit?")
-    egymasmelleennyit = int(input())
-else:
-    egymasmelleakar == False
-    print("ok akkor mennyit")
-    mennyitakarsz = int(input())
+teljes = feltolto()
 
-print("Milyen fajta jegyet szeretnél? (d - diák/nyugdíjas, f - felnőtt, gy - gyerek)")
-milyen = input()
+def kiir(nezoter):
+    for oldal in nezoter:
+        for sor in oldal:
+            print(sor)
+        print("kövi")
 
-szamolos = 0
-if egymasmelleakar == False:
-    for t in teljes:
-        for l in t:
-             for q in l:
-                if q == 0 and szamolos != mennyitakarsz:
-                     print("Van szabadhely")
-                     szamolos += 1 
-
-elif egymasmelleakar == True:
-    for t in teljes:
-        for l in t:
-            for q in l:
-                if szamolos != egymasmelleennyit:
-                    if q == 0:
-                        szamolos += 1
-                    else:
-                        szamolos == 0
-    if szamolos == egymasmelleennyit:
-        print('asd')
+kiir(teljes)
