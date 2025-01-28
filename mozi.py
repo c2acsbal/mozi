f536081a7849ba07d6ee9df0c9130ddf207f2ec7
c2acsbal <c2acsbal@vasvari.org>
2025-01-28 21:27:30 +0100

    feltolto -és kiir függvény megcsinálása és tökéletesítése

--- mozi.py
+++ mozi.py
@@ -1,22 +1,24 @@
 import random
-teljes = []
-nezoterbal = []
-nezoterjobb = []
-sor = []
-sor2 = []
+
 def feltolto():
-    while len(nezoterbal) != 15:
+    sor = []
+    sor2 = []
+    nezoterbal = []
+    nezoterjobb = []
+    teljes = []
+    while len(nezoterbal) != 16:
         for i in range(10):
             randi = random.randint(0,4)
             sor.append(randi)
-
         nezoterbal.append(sor)
-    while len(nezoterjobb) != 15:
+        sor = []
+
+    while len(nezoterjobb) != 16:
         for t in range(10):
             randi2 = random.randint(0,4)
             sor2.append(randi2)
-        
         nezoterjobb.append(sor2)
+        sor2 = []
     
     teljes.append(nezoterbal)
     teljes.append(nezoterjobb)
@@ -25,9 +27,7 @@ def feltolto():
 teljes = feltolto()
 
 def kiir(nezoter):
-    for oldal in nezoter:
-        for sor in oldal:
-            print(sor)
-        print("kövi")
+    for i in range(len(nezoter[0])-1):
+            print(nezoter[0][i], str(i+1).zfill(2), nezoter[1][i])
 
 kiir(teljes)
